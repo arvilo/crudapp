4d33431045111c76415c9a38e9cd0038e6593223
Nijat Aliyev <nicat3699@gmail.com>
2025-02-22 00:54:10 +0400

    format syntax

--- src/main/java/az/arvilo/crudapp/Data.java
+++ src/main/java/az/arvilo/crudapp/Data.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Data {
 
-
     /**
      * A static field that stores all database tables in memory.
      * - The key represents the table name.
@@ -16,5 +15,6 @@ public class Data {
      * - Null values are not allowed anywhere.
      * - Duplicate table names are not allowed.
      */
-    public static final HashMap<String,List<List<String>>> TABLES = new HashMap<>();
+    public static final HashMap<String, List<List<String>>> TABLES = new HashMap<>();
+
 }
