bbde19de6855aed29e3501f93b4b170eecdfa777
Nijat Aliyev <nicat3699@gmail.com>
2025-02-24 18:47:36 +0400

    create exception classes

---
+++ src/main/java/az/arvilo/crudapp/exception/DataBaseCorruptException.java
@@ -0,0 +1,7 @@
+package az.arvilo.crudapp.exception;
+
+public class DataBaseCorruptException extends Exception {
+    public DataBaseCorruptException(String message) {
+        super(message);
+    }
+}
